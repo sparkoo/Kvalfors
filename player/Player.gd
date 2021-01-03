05b36f40aaea8dbf63d8fc16508ffd03b742448d
Michal Vala <michal.vala@gmail.com>
2021-01-03 13:32:03 +0100

    custom model in scene

--- player/Player.gd
+++ player/Player.gd
@@ -3,9 +3,9 @@ extends KinematicBody
 export var SPEED = 10
 export var SIDE_SPEED = 10
 export var JUMP_SPEED = 10
-export var SLIDE_HEIGHT = .5
-export var NORMAL_HEIGHT = 1
-export var JUMP_HEIGHT = 1.5
+export var NORMAL_HEIGHT = .7
+var SLIDE_HEIGHT = NORMAL_HEIGHT / 2
+var JUMP_HEIGHT = NORMAL_HEIGHT * 2
 
 const LINE_WIDTH = 1
 
@@ -27,10 +27,10 @@ func move():
 		var direction = Input.get_action_strength("left") - Input.get_action_strength("right")
 		translation.x = LINE_WIDTH * direction
 		
-		if Input.is_action_just_pressed("jump") and translation.y == NORMAL_HEIGHT:
+		if Input.is_action_just_pressed("jump") and compare_floats(translation.y, NORMAL_HEIGHT):
 			translation.y = JUMP_HEIGHT
 			$JumpSlideTimer.start()
-		elif Input.is_action_just_pressed("slide") and translation.y == NORMAL_HEIGHT:
+		elif Input.is_action_just_pressed("slide") and compare_floats(translation.y, NORMAL_HEIGHT):
 			translation.y = SLIDE_HEIGHT
 			$JumpSlideTimer.start()
 		move_and_slide(motion)
@@ -47,3 +47,8 @@ func hit():
 
 func _input(event):
 	pass
+
+const FLOAT_EPSILON = 0.00001
+
+static func compare_floats(a, b, epsilon = FLOAT_EPSILON):
+	return abs(a - b) <= epsilon
